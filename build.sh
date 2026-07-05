98fa5c646a9c517c9e52eeab9494f6019e2407fa
bishal <itsmebishal35@gmail.com>
2026-07-05 13:33:39 +0545

    user

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
+set -o errexit
+
 pip install -r requirements.txt
 cd waiter_app
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py seed
+echo "
+from django.contrib.auth.models import User
+from core.models import UserProfile
+if not User.objects.filter(username='admin').exists():
+    u = User.objects.create_superuser('admin', '', 'Bishal@2030')
+    p, _ = UserProfile.objects.get_or_create(user=u)
+    p.role = 'admin'
+    p.save()
+    print('Admin created!')
+else:
+    u = User.objects.get(username='admin')
+    p, _ = UserProfile.objects.get_or_create(user=u)
+    p.role = 'admin'
+    p.save()
+    print('Admin role updated!')
+" | python manage.py shell
